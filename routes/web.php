6dade87c9887208602896b26814b58d5df01a888
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2018-02-02 20:16:36 +0100

    Clase 9

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,15 @@ Route::get('/', function () {
 Route::get('admin', function () {
     return view('admin.dashboard');
 });
+
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
