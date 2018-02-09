0de8ab8024c67a2d6a930a426444ca83d7f12e0c
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2018-02-09 20:38:36 +0100

    Clase 23

---
+++ resources/views/partials/social-links.blade.php
@@ -0,0 +1,30 @@
+<div class="buttons-social-media-share">
+    <ul class="share-buttons">
+        <li>
+            <a href="https://www.facebook.com/sharer.php?u={{ request()->fullUrl() }}&title={{ $description }}"
+               title="Compartir en Facebook" target="_blank">
+                <img alt="Share on Facebook"
+                     src="/img/flat_web_icon_set/Facebook.png">
+            </a>
+        </li>
+        <li>
+            <a href="https://twitter.com/intent/tweet?url={{ request()->fullUrl() }}&text={{ $description }}&via=CarlosAbris&hashtags=BlogIngeniero"
+               target="_blank" title="Tweet"><img
+                        alt="Tweet" src="/img/flat_web_icon_set/Twitter.png">
+            </a>
+        </li>
+        <li>
+            <a href="https://plus.google.com/share?url={{ request()->fullUrl() }}" target="_blank"
+               title="Compartir en Google+">
+                <img alt="Share on Google+"
+                     src="/img/flat_web_icon_set/Google+.png">
+            </a>
+        </li>
+        <li>
+            <a href="https://pinterest.com/pin/create/bookmarklet/?url={{ request()->fullUrl() }}&description={{ $post->title }}"
+               target="_blank" title="Pin it">
+                <img alt="Pin it" src="/img/flat_web_icon_set/Pinterest.png">
+            </a>
+        </li>
+    </ul>
+</div>
