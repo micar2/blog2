8f184edbd42da8bd0f0b1bb407911cce90591583
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2018-02-23 19:01:38 +0100

    Clase 49

---
+++ resources/views/posts/photo.blade.php
@@ -0,0 +1,6 @@
+<figure>
+    <img src="{{ Storage::url($post->photos()->first()->url) }}"
+         alt="Foto: {{ $post->title }}"
+         class="img-responsive"
+    >
+</figure>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,28 +6,9 @@
 
 @section('content')
     <article class="post container">
-        @if ($post->photos->count() === 1)
-            <figure>
-                <img src="{{ Storage::url($post->photos()->first()->url) }}" alt="" class="img-responsive">
-            </figure>
-        @elseif ($post->photos->count() > 1)
-            @include('posts.carousel')
-        @elseif ($post->iframe)
-            <div class="video">
-                {!! $post->iframe !!}
-            </div>
-        @endif
+        @include($post->viewType())
         <div class="content-post">
-            <header class="container-flex space-between">
-                <div class="date">
-                    <span class="c-gris">{{ optional($post->published_at)->format('M d') }} / {{ $post->owner->name }}</span>
-                </div>
-                @if ($post->category)
-                <div class="post-category">
-                    <span class="category">{{ $post->category->name }}</span>
-                </div>
-                @endif
-            </header>
+            @include('posts.header')
             <h1>{{ $post->title }}</h1>
             <div class="divider"></div>
             <div class="image-w-text">
@@ -36,11 +17,7 @@
 
             <footer class="container-flex space-between">
                 @include('partials.social-links', ['description' => $post->title])
-                <div class="tags container-flex">
-                    @foreach($post->tags as $tag)
-                        <span class="tag c-gris">#{{ $tag->name }}</span>
-                    @endforeach
-                </div>
+                @include('posts.tags')
             </footer>
         </div>
     </article>
